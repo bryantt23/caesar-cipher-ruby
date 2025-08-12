254ea0c6388ecd6355594ac3a37aa3fed41af6a4
bryant tunbutr <bryantt23@users.noreply.github.com>
2025-08-13 02:41:23 +0800

    easy test

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -1,2 +1,25 @@
+def is_alpha?(char)
+  char.match?(/[A-Za-z]/)
+end
 
+def caesar_cipher(message, shift)  
+  output=""
+  message.each_char do |char|
+    puts char
+    if is_alpha?(char)
+      val=char.ord
+      if val>="a".ord
+        next_val=(val+shift)%"z".ord
+        output+=(next_val).chr      
+      else
+        output+=((val+shift)%65).chr
+      end
+      # output+=val.chr
+    else
+      output+=char
+    end
+  end  
+  puts output
+  output
+end
 
--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -5,4 +5,16 @@ RSpec.describe "Caesar Cipher" do
   it "runs hello world test" do
     expect("hello").to eq("hello")
   end
+  
+  it "easy test" do 
+    expect(caesar_cipher("abc", 1)).to eq("bcd")
+  end
+  
+  xit "easy test capitalized" do 
+    expect(caesar_cipher("ABC", 1)).to eq("BCD")
+  end
+
+  xit "passes a baseline test" do 
+    expect(caesar_cipher("What a string!", 5)).to eq("Bmfy f xywnsl!")
+  end
 end
