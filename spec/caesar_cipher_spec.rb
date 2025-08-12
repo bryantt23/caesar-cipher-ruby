2dc5403fa36697afa077ed356fbd1a94557820c9
bryant tunbutr <bryantt23@users.noreply.github.com>
2025-08-13 03:11:20 +0800

    more tests

--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -15,4 +15,21 @@ RSpec.describe "Caesar Cipher" do
   it "passes a baseline test" do 
     expect(caesar_cipher("What a string!", 5)).to eq("Bmfy f xywnsl!")
   end
+
+    it "keeps non-letters unchanged" do
+    expect(caesar_cipher("Zebra-493?", 3)).to eq("Cheud-493?")
+  end
+
+  it "preserves case" do
+    expect(caesar_cipher("xyz XYZ", 2)).to eq("zab ZAB")
+  end
+
+  it "wraps around z/Z correctly" do
+    expect(caesar_cipher("Wrap from z and Z.", 1)).to eq("Xsbq gspn a boe A.")
+  end
+
+  it "does nothing with a 26 shift" do
+    expect(caesar_cipher("abc ABC", 26)).to eq("abc ABC")
+  end
+
 end
